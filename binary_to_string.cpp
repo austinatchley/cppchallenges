c8d51e6b9092c59313a9ef837755fd0b27dc5c8a
Austin Atchley <austinatchley@gmail.com>
2019-12-06 19:03:09 -0600

    binary_to_string

---
+++ binary_to_string.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cinttypes>
+#include <vector>
+
+template <typename iter>
+std::string convert(iter begin, iter end) {
+    std::string out = "";
+    for (T elem : v) {
+        
+    }
+}
+
+int main(int argc, char** argv) {
+    using namespace std;
+
+    vector<uint8_t> v = {40, 65, 20};
+    cout << convert<uint8_t>(v.begin(), v.end()) << endl;
+}
